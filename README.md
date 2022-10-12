e7feaf5c498c6936769e3d35b917c4d7f7a3f12e
CoderGirl <angi.adema@gmail.com>
2022-10-12 12:44:41 -0600

    added write and initialize functions to index.js and badge functions to generateMarkdown file.

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# README-Generator
+
+  # kjkj
+
+  ## Description
+
+  kjkj
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Collaborator](#collaborator)
+  - [Test](#test)
+  - [License](#license)
+  - [Questions](#questions)
+
+  ## Usage
+  kjkj
+  
+
+  ## License
+
+  Application includes the MIT license
+
+  ## Questions
+  Author: kjkj and link kjk
+  For questions, please feel free to contact me at kjj
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,33 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license ==="No License") {
+    return '';
+  } 
+  return 
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license ==="No License") {
+    return '';
+  } 
+  return ' - [License](#license)'
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license ==="No License") {
+    return '';
+  } 
+  return `
+  ## License
+
+  Application includes the ${license} license`
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -16,23 +35,26 @@ function generateMarkdown(data) {
   # ${data.projectTitle}
 
   ## Description
+
   ${data.description}
 
   ## Table of Contents
-  -Usage
-  -License
-  -Installation Testing
-  -Questions
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Collaborator](#collaborator)
+  - [Test](#test)
+ ${renderLicenseLink(data.license)}
+  - [Questions](#questions)
 
   ## Usage
   ${data.usage}
   
-  ## License
-  Application includes the ${data.license} license
+${renderLicenseSection(data.license)}
 
   ## Questions
   Author: ${data.githubUsername} and link ${data.githubLink}
-  For questions please contact me at ${data.email}
+  For questions, please feel free to contact me at ${data.email}
 `;
 }
 
