fa1133cf023c565e20a77abbe65b60d114aa80f6
CoderGirl <angi.adema@gmail.com>
2022-10-13 15:14:40 -0600

    updated the generateMarkdown with the license badge options code and the info links.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,28 +1,82 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license ==="No License") {
-    return '';
-  } 
-  return 
+  // if (license ==="No License") {
+  //   return '';
+  // } 
+  // return 
+  const label = 'licenseTitle';
+  let licenseOption = license;
+  let message = '';
+  let color = '';
+  let badgeURL = '';
 
+    switch (licenseOption) {
+      case 'MIT':
+        message = 'MIT';
+        color = 'brightgreen';
+        badgeURL = `https://img.shields.io/badge/${label}-${message}-${color}`;
+        break;
+      case 'Apache 2.0':
+        message = 'Apache 2.0';
+        color = 'critical';
+        badgeURL = `https://img.shields.io/badge/${label}-${message}-${color}`;
+        break;
+      case 'GPL':
+        message = 'GPL';
+        color = 'blue';
+        badgeURL = `https://img.shields.io/badge/${label}-${message}-${color}`;
+        break;
+      case 'BSD':
+        message = 'BSD';
+        color = 'orange';
+        badgeURL = `https://img.shields.io/badge/${label}-${message}-${color}`;
+        break;
+      default:
+        message = 'No License';
+        color = 'inactive';
+        badgeURL = `https://img.shields.io/badge/${label}-${message}-${color}`;
+        break;
+    }
+    return badgeURL;
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license ==="No License") {
-    return '';
-  } 
-  return ' - [License](#license)'
+  // if (license ==="No License") {
+  //   return '';
+  // } 
+  let licenseOption = license;
+
+  switch (licenseOption) {
+    case 'MIT':
+      linkURL = 'https://opensource.org/licenses/MIT';      
+      break;
+    case 'Apache 2.0':
+      linkURL = 'https://opensource.org/licenses/Apache-2.0';      
+      break;
+    case 'GPL':
+      linkURL = 'https://opensource.org/licenses/GPL-3.0';
+      break;
+    case 'BSD':
+      linkURL = 'https://opensource.org/licenses/BSD-3-Clause';
+      break;  
+    default:
+      linkURL = 'NONE';
+      break;
+  }
+  return linkURL;
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license ==="No License") {
-    return '';
-  } 
+  // if (license ==="No License") {
+  //   return '';
+  // } 
+  let licenseBadge = renderLicenseBadge(license);
+  let licenseInfo = renderLicenseLink(license);
   return `
   ## License
 
@@ -48,10 +102,20 @@ function generateMarkdown(data) {
   - [Credits](#credits) 
   - [Questions](#questions)
 
+  ## Installation
+
   ## Usage
   ${data.usage}
+
+  ## Contribution
+
+  ## Test
+
+  ## License
   
 ${renderLicenseSection(data.license)}
+
+  ## Credits
 
   ## Questions
   GitHub: (https://github.com/${data.githubUsername})
